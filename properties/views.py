3bbc4ed9325b01b982ae931169cc1ff5daf36b60
Rioba-Ian <riobafelix@gmail.com>
2025-09-06 10:05:27 +0300

    use Json response to get the response

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,6 @@
 from serializers import PropertySerializer
+from django.http import JsonResponse
 from properties.models import Property
-from rest_framework.response import Response
 from django.views.decorators.cache import cache_page
 
 # Create your views here.
@@ -10,4 +10,4 @@ from django.views.decorators.cache import cache_page
 def property_list(request):
     properties = Property.objects.all()
     serializer = PropertySerializer(properties, many=True)
-    return Response(serializer.data)
+    return JsonResponse(serializer.data, safe=False)
