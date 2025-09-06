6894d1030e6c8c954b34b3dd05f709a92d876a3b
Rioba-Ian <riobafelix@gmail.com>
2025-09-06 10:12:04 +0300

    use get_all_properties from utils

--- properties/views.py
+++ properties/views.py
@@ -1,13 +1,17 @@
 from serializers import PropertySerializer
 from django.http import JsonResponse
-from properties.models import Property
+
+# from properties.models import Property
 from django.views.decorators.cache import cache_page
+from properties.utils import get_all_properties
 
 # Create your views here.
 
 
 @cache_page(60 * 15)  # Cache the view for 15 minutes
 def property_list(request):
-    properties = Property.objects.all()
-    serializer = PropertySerializer(properties, many=True)
-    return JsonResponse({"properties": serializer.data}, safe=False)
+    if request.method == "GET":
+        properties = get_all_properties()
+        serializer = PropertySerializer(properties, many=True)
+        return JsonResponse({"properties": serializer.data}, safe=False)
+    return JsonResponse({"error": "Method not allowed"}, status=405)
